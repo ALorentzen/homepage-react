c509e87268e1097bba32e9a5e8271ca7f261ee9d
Anders Lorentzen <73347557+ALorentzen@users.noreply.github.com>
2022-01-22 13:44:43 +0100

    structural changes to webDevelopment.js . alternative structure for readability. edited alt-property.

--- src/components/pages/webDevelopment/WebDevelopment.js
+++ src/components/pages/webDevelopment/WebDevelopment.js
@@ -22,92 +22,176 @@
                 </div>
 
                 <div className='card-container'>
-                    <Link className='travel--link' to='/TravelForm'>
-                        <h1 className='link--header'>Travel form</h1>
-                        <img 
-                            className='travel--container--bckr'
-                            src='/images/background_images/photo-1436491865332-7a61a109cc05.jpg' 
-                            alt='flight'
-                        /> 
+                    <Link 
+                        className='travel--link' 
+                        to='/TravelForm'>
+                            <h1 
+                                className='link--header'>
+                                    Travel form
+                            </h1>
+                            <img 
+                                className='travel--container--bckr'
+                                src='/images/background_images/photo-1436491865332-7a61a109cc05.jpg' 
+                                alt='photo14364918653327a61a109cc05'
+                            /> 
                     </Link>
                 </div>
 
-                <div id="card--container--meme--div" className='card-container'>
-                    <Link className='meme-link' to='/MemeGenerator'>
-                        <h2 className='meme--h2'>meme generator</h2>
-                        <img  className='meme-logo-img'src="/images/unsorted/rinrrbyBT.png" />
+                <div 
+                    id="card--container--meme--div" 
+                    className='card-container'
+                >
+                    <Link 
+                        className='meme-link' 
+                        to='/MemeGenerator'
+                    >
+                        <h2 
+                            className='meme--h2'>
+                                meme generator
+                        </h2>
+                        <img  
+                            className='meme-logo-img'
+                            src="/images/unsorted/rinrrbyBT.png"
+                            alt="rinrrbyBT.png" 
+                        />
                     </Link>
                 </div>
 
-                <div className='card-container'>
+                <div 
+                    className='card-container'
+                >
                     <CTA>
-                        <h1>This is an important CTA</h1>
-                        <button>Click me!</button>
+                        <h1>
+                            This is an important CTA
+                        </h1>
+                        <button>
+                            Click me!
+                        </button>
                     </CTA>
                     <CTA>
-                        <form className='cta-form'>
-                            <input type="email" placeholder="Enter email address here"/>
+                        <form 
+                            className='cta-form'
+                        >
+                            <input 
+                                type="email" 
+                                placeholder="Enter email address here"
+                            />
                             <br />
-                            <button>Submit</button>
+                            <button>
+                                Submit
+                            </button>
                         </form>
                     </CTA>
                 </div>
 
-                <div className='card-container'>
+                <div 
+                    className='card-container'
+                >
                     <Callout>
-                        <h2>This is a {`<Callout>`} component w/ React children </h2>
-                        <p>Email, Image and Info </p>
+                        <h2>
+                            This is a {`<Callout>`} component w/ React children 
+                        </h2>
+                        <p>
+                            Email, Image and Info 
+                        </p>
                     </Callout>
 
                     <Callout>
-                        <img src="https://picsum.photos/id/102/4320/3240" width="100%" alt=''/>
-                        <figcaption>Just look at those sparkling raspberries!</figcaption>
+                        <img 
+                            src="https://picsum.photos/id/102/4320/3240" 
+                            width="100%" 
+                            alt='https://picsum.photos/id/102/4320/3240'/>
+                        <figcaption>
+                            Just look at those sparkling raspberries!
+                        </figcaption>
                     </Callout>
 
                     <Callout>
-                        <h2>Give us your email. We definitely won't sell it to anyone.</h2>
-                        <input className='callout-email' type="email" placeholder="Enter Email"/>
-                        <button>Sign me up!</button>
+                        <h2>
+                            Give us your email. We definitely won't sell it to anyone.
+                        </h2>
+                        <input 
+                            className='callout-email' 
+                            type="email" 
+                            placeholder="Enter Email"
+                        />
+                        <button>
+                            Sign me up!
+                        </button>
                     </Callout>
-
                 </div>
 
                 <div className='card-container'>
                     <RoundedImg 
                         className='rounded-img-container' 
                         src='./images/portfolio/characters/whistle.png' 
+                        alt='roundedImg' 
                         borderRadius={80} 
                     />
                 </div>
 
-                <div id="todo--container" className='card-container'>
+                <div 
+                    id="todo--container" 
+                    className='card-container'
+                >
                     <Todo />
                 </div>
 
-                <div className='card-container'>
+                <div 
+                    className='card-container'
+                >
                     <Counter />
                 </div>
 
-                <div className='card-container'>
-                    <Link className='airBnb-link' to='/TravelComponent'>
-                        <img className='airBnb-link--img' src='./images/icons/Airbnb_Logo_Bélo.svg.png' alt='airBnb-link'/>
+                <div 
+                    className='card-container'
+                >
+                    <Link 
+                        className='airBnb-link' 
+                        to='/TravelComponent'
+                    >
+                        <img 
+                            className='airBnb-link--img' 
+                            src='./images/icons/Airbnb_Logo_Bélo.svg.png' 
+                            alt='airBnb-link'
+                        />
                     </Link>
                 </div>
                 
-                <div className='card-container'>
-                    <AddItem className='addItem--container'/>
+                <div 
+                    className='card-container'
+                >
+                    <AddItem 
+                        className='addItem--container'
+                    />
                </div>
 
-               <div className='card-container'>
+               <div 
+                    className='card-container'
+                >
                     <ChangeBoxes />
                 </div>
 
-                <div className='card-container'>
-                    <Link className='form--component--link' to='/Form' >Form</Link> 
+                <div 
+                    className='card-container'
+                >
+                    <Link 
+                        className='form--component--link' 
+                        to='/Form' 
+                    >
+                        Form
+                    </Link> 
                 </div>
 
-                <div className='card-container'>
-                    <Link className='form--component--link' to='/ExerciseForm' >test form</Link> 
+                <div 
+                    className='card-container'
+                >
+                    <Link 
+                        className='form--component--link' 
+                        to='/ExerciseForm' 
+                    >
+                        test form
+                    </Link> 
                 </div>
             </div>
         )
